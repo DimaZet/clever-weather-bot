378c9212694e40bb6e16c81889f8cf2bb6728a7d
DimaZet <vasilisk98@gmail.com>
2020-04-12 14:26:31 +0300

    mv methods in class

--- decoder.py
+++ decoder.py
@@ -2,26 +2,28 @@ import os
 
 import requests
 
-token = os.environ['GEOCODER_TOKEN']
 
+class Decoder:
 
-def prepare(address: str) -> str:
-    return address  # TODO
+    def __init__(self):
+        self.token = os.environ['GEOCODER_TOKEN']
 
+    def __prepare__(self, address: str) -> str:
+        return address  # TODO
 
-def decode(address: str) -> dict:
-    resp = requests.get(
-        url='https://geocode-maps.yandex.ru/1.x',
-        params={
-            'format': 'json',
-            'apikey': token,
-            'geocode': prepare(address)
-        }
-    ).json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-    pos = [float(x) for x in resp.split()]
-    return {'lon': pos[0], 'lat': pos[1]}
+    def decode(self, address: str) -> dict:
+        resp = requests.get(
+            url='https://geocode-maps.yandex.ru/1.x',
+            params={
+                'format': 'json',
+                'apikey': self.token,
+                'geocode': self.__prepare__(address)
+            }
+        ).json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+        pos = [float(x) for x in resp.split()]
+        return {'lon': pos[0], 'lat': pos[1]}
 
 
 if __name__ == '__main__':
-    r = decode('Москва+Льва+Толстого+16')
+    r = Decoder().decode('Москва+Льва+Толстого+16')
     print(r['lon'], r['lat'])
--- forecasting.py
+++ forecasting.py
@@ -2,23 +2,26 @@ import os
 
 import requests
 
-token = os.environ['WEATHER_TOKEN']
 
+class Weather:
 
-def get_weather(lat: float, lon: float, offset: int) -> (int, int):
-    day = requests.get(
-        url='https://api.weather.yandex.ru/v1/forecast',
-        params={
-            'lat': lat,
-            'lon': lon
-        },
-        headers={
-            'X-Yandex-API-Key': token
-        }
-    ).json()['forecasts'][offset]['day']['day']
-    return day['temp_avg'], day['feels_like']
+    def __init__(self):
+        self.token = os.environ['WEATHER_TOKEN']
+
+    def get_weather(self, lat: float, lon: float, offset: int) -> (int, int):
+        day = requests.get(
+            url='https://api.weather.yandex.ru/v1/forecast',
+            params={
+                'lat': lat,
+                'lon': lon
+            },
+            headers={
+                'X-Yandex-API-Key': self.token
+            }
+        ).json()['forecasts'][offset]['day']['day']
+        return day['temp_avg'], day['feels_like']
 
 
 if __name__ == '__main__':
-    avg, feels = get_weather(55.45, 37.37, 1)
+    avg, feels = Weather().get_weather(55.45, 37.37, 1)
     print(avg, feels)
